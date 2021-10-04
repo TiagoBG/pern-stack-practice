6c007824f14a60619d048c568d832c10dd25d986
Santiago Betancur Graciano <santybg_94@hotmail.com>
2021-10-03 22:12:17 -0500

    Se crea la base del cliente con React y la base del server con Express y Hapi

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       BASE     
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ server/hapiapp/index.js
@@ -0,0 +1,27 @@
+const Hapi = require ('@hapi/hapi');
+
+const init = async() => {
+
+    const server = Hapi.Server({
+        port: 8000,
+        host: 'localhost'
+    });
+
+    server.route({
+        method: 'GET',
+        path: '/',
+        handler: (request, h) => {
+            return 'Hello World from Hapi!';
+        }
+    });
+
+    await server.start();
+    console.log('Server running on %s', server.info.uri);
+}
+
+process.on('unhandledRejection', (err)=>{
+    console.log(err);
+    process.exit(1);
+});
+
+init();

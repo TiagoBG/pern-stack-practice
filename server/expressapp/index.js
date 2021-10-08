c76a8e631e4d31def94a55d2a32627b032fb5161
Santiago Betancur Graciano <santybg_94@hotmail.com>
2021-10-07 20:01:39 -0500

    Se inicializa enrutamiento

--- server/expressapp/index.js
+++ server/expressapp/index.js
@@ -2,10 +2,14 @@ const express = require('express');
 const app = express();
 const port = 8000;
 
-app.get('/', (request, response)=>{
-    response.send('Hello World from Express!')
-});
+//middlewares
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
 
+//routes
+app.use(require('./src/routes/routes'));
+
+//listen port
 app.listen(port, ()=>{
     console.log(`Example app listening at http://localhost:${port}`)
 });
